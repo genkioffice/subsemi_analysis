f5c70e7ec097379e3535bf387a228efce703e5e4
Genki Takahashi <genki.takahashi.contact@gmail.com>
2018-06-12 00:30:14 +0900

    finish creating plot with treemap library

---
+++ first/README.md
@@ -0,0 +1,2 @@
+Inside this directory, the original data from e-stats saved in 2015_population_modified.csv, and the modified file of it is 2015_population_modified.csv. Chap1_hw.R is main script text file. Tutorial data of 1st chapter in the textbook is in cv.csv. till_get_numeric_population.RDATA is just an experimental variable sets for HW in 1st week.
+
--- first/chap1_hw.R
+++ first/chap1_hw.R
@@ -3,7 +3,8 @@ print(getwd())
 
 #set libraries
 print(search())
-library("gdata") #to read xls data
+library(gdata) #to read xls data
+library(treemap) #to use treemap plot... you may have to install ggplot2 & treemap and some related libraries
 
 #load data
 population = read.xls("2015_population.xls")
@@ -13,5 +14,17 @@ c_name = c("state","city","population") #to set columns name
 names(population) <- c_name
 print(names(population))
 
+#modify population column
 population <- population[11:length(population[[2]]),] #delete useless values
-sapply(gsub(',','',population$population),function(x) x[1]) # to eliminate comma on population column
+population$population = sapply(gsub(',','',population$population),function(x) x[1]) # to eliminate comma on population column
+population$population = as.numeric(population$population)#to change character d-type into numeric d-type
+
+#make treemap
+states = population$state #to reshape population dataframe
+population_without_allState <- subset(population, !(city %in% states)) # delete rows where state and city have a same name
+treemap(population_without_allState,
+        index=c("state","city"),vSize = "population", title = "Distribution of the population in Japan",type = "index",fontsize.labels = c(12,8),
+        fontcolor.labels = c("white","orange"),fontface.labels = c(2,1),bg.labels =c("transparent"),align.labels = list(c("center","center"),c("right","bottom")),
+        overlap.labels = 0.5,inflate.labels = F, fontfamily.title ="HiraKakuPro-W3", fontfamily.labels = "HiraKakuPro-W3",
+        fontfamily.legend = "HiraKakuPro-W3" )
+
